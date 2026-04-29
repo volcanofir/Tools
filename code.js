669bf2c4092709c40bc718143cf5488c12509883
volcanofir <volcanofir@gmail.com>
2026-04-29 23:41:59 +0800

    Add files via upload

---
+++ code.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = ‘tool-cache-v1’;
+
+const FILES_TO_CACHE = [
+‘/Tools/’,
+‘/Tools/index.html’,
+‘/Tools/price.html’,
+‘/Tools/tax.html’
+];
+
+self.addEventListener(‘install’, event => {
+event.waitUntil(
+caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+);
+self.skipWaiting();
+});
+
+self.addEventListener(‘activate’, event => {
+event.waitUntil(
+caches.keys().then(keys =>
+Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+)
+);
+self.clients.claim();
+});
+
+self.addEventListener(‘fetch’, event => {
+event.respondWith(
+caches.match(event.request).then(cached => {
+return cached || fetch(event.request).then(response => {
+return caches.open(CACHE_NAME).then(cache => {
+cache.put(event.request, response.clone());
+return response;
+});
+});
+}).catch(() => caches.match(’/Tools/index.html’))
+);
+});
